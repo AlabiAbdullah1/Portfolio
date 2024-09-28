ba68a8ff7ae58cf2421cf3216ae93e3c799fc4e0
Alabiabdullahi <abdullahialabi234@gmail.com>
2024-09-29 00:57:19 +0100

    logo and title update

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,12 +1,9 @@
-function Loading() {
+function Loader() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <div className="loader bg-indigo-500 h-10 w-10 rounded-full animate-spin-move"></div>
-      <h2 className="mt-4 text-lg font-semibold text-indigo-600">
-        Please Wait...
-      </h2>
+    <div className="absolute inset-0 bg-slate-200/20 backdrop-blur-sm flex items-center justify-center">
+      <div className="loader"></div>
     </div>
   );
 }
 
-export default Loading;
+export default Loader;
